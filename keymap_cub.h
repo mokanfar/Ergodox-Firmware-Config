cf1802f407b5055ad629e0292c5e4c4717000e0a
mokanfar <pharmokan@gmail.com>
2015-09-04 14:24:58 -0700

    add third layer

--- keymap_cub.h
+++ keymap_cub.h
@@ -30,7 +30,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        TRNS,
                             BTN1, TRNS,TRNS,
     // right hand
-         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+         FN1,FN2,TRNS,TRNS,TRNS,TRNS,TRNS,
          F15,TRNS,  P7,  P8,  P9,  TRNS,TRNS,
              TRNS,  P4,  P2,  P6,  TRNS,TRNS,
          F16,TRNS,  P1,  P2,  P3,  TRNS,TRNS,
@@ -50,7 +50,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS,
                                  TRNS,FN0,FN31,
         // right hand
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+           FN3,FN4,TRNS,TRNS,TRNS,TRNS,TRNS,
            TRNS,FN20,FN21,UP,FN22,FN23,FN2,
                 HOME,LEFT,DOWN,RIGHT,END,TRNS,
            TRNS,FN27,FN24,FN28,FN29,TRNS,TRNS,
@@ -58,7 +58,27 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         F19,TRNS,
         F23,
         F24,TRNS,TRNS
-    )
+    ),
+        KEYMAP(  // LayerN: fully transparent
+        // left hand
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,TRNS,
+        // right hand
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                       TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
 };
 
 /*
@@ -67,40 +87,35 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //Default
 static const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_LAYER_TOGGLE(4),
-    [1]  = ACTION_LAYER_MOMENTARY(1),
     [3]  = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC), // {
-    [4]  = ACTION_LAYER_MOMENTARY(2), //photoshop layer
     [5]  = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC), //}
     [6]  = ACTION_MODS_KEY(MOD_LSFT, KC_0), //)
     [8]  = ACTION_MODS_KEY(MOD_LCTL, KC_SCLN), //Launchy Ctrl+;
     [9]  = ACTION_LAYER_TAP_KEY(1, KC_F), //Mouse Layer or D
-    //[10] = ACTION_LAYER_TAP_KEY(4, KC_L), //sublime right hand
     [11] = ACTION_LAYER_TAP_KEY(2, KC_SPC), //sublime expert layer
 };
 
 //Mouse Layer
 static const uint16_t PROGMEM fn_actions_1[] = {
-    [1]  = ACTION_MODS_KEY(MOD_LGUI, KC_LEFT), //ctrl+shift+n
-    [2]  = ACTION_MODS_KEY(MOD_LGUI, KC_RIGHT), //ctrl+shift+n
-    [6]  = ACTION_MODS_KEY(MOD_LCTL, KC_HOME), //r
-    [7]  = ACTION_MODS_KEY(MOD_LCTL, KC_END), //t works
+    [1]  = ACTION_MODS_KEY(MOD_LGUI, KC_LEFT),
+    [2]  = ACTION_MODS_KEY(MOD_LGUI, KC_RIGHT),
+    [6]  = ACTION_MODS_KEY(MOD_LCTL, KC_HOME),
+    [7]  = ACTION_MODS_KEY(MOD_LCTL, KC_END),
 };
 
 //sublime expert layer
 static const uint16_t PROGMEM fn_actions_2[] = {
-    //[0] = ACTION_LAYER_MOMENTARY(0),
-    [0] = ACTION_MODS_KEY(MOD_LCTL,KC_D),
-    [1] = ACTION_LAYER_SET_CLEAR(0),
-    [2] = ACTION_LAYER_OFF(4, ON_BOTH),
-    [3] = ACTION_MODS_KEY(MOD_LGUI,KC_G),
-    [4] = ACTION_MODS_KEY(MOD_LCTL|MOD_LSFT,KC_G),
-    [5] = ACTION_MODS_KEY(MOD_LALT,KC_E),
-    [6] = ACTION_MODS_KEY(MOD_LCTL|MOD_LSFT,KC_SCLN),
-    [7] = ACTION_MODS_KEY(MOD_LCTL,KC_T),
-    [8] = ACTION_MODS_KEY(MOD_LALT,KC_MINS),
-    [9] = ACTION_MODS_KEY(MOD_LCTL,KC_A),
-    [10] = ACTION_MODS_KEY(MOD_LCTL,KC_S),
+    [0]  = ACTION_MODS_KEY(MOD_LCTL|MOD_LCTL,KC_K),
+    [1]  = ACTION_LAYER_SET_CLEAR(0),
+    [2]  = ACTION_LAYER_OFF(4, ON_BOTH),
+    [3]  = ACTION_MODS_KEY(MOD_LGUI,KC_G),
+    [4]  = ACTION_MODS_KEY(MOD_LCTL|MOD_LSFT,KC_G),
+    [5]  = ACTION_MODS_KEY(MOD_LALT,KC_E),
+    [6]  = ACTION_MODS_KEY(MOD_LCTL|MOD_LSFT,KC_SCLN),
+    [7]  = ACTION_MODS_KEY(MOD_LCTL,KC_T),
+    [8]  = ACTION_MODS_KEY(MOD_LALT,KC_MINS),
+    [9]  = ACTION_MODS_KEY(MOD_LCTL,KC_A),
+    [10] = ACTION_LAYER_TAP_KEY(3, KC_S), //sublime expert layer
     [12] = ACTION_MODS_KEY(MOD_LCTL|MOD_LSFT,KC_LBRC),
     [13] = ACTION_MODS_KEY(MOD_LCTL,KC_G),
     [14] = ACTION_MODS_KEY(MOD_LCTL,KC_Z),
@@ -126,6 +141,7 @@ static const uint16_t PROGMEM fn_actions_2[] = {
 #define FN_ACTIONS_SIZE     (sizeof(fn_actions)   / sizeof(fn_actions[0]))
 #define FN_ACTIONS_1_SIZE   (sizeof(fn_actions_1) / sizeof(fn_actions_1[0]))
 #define FN_ACTIONS_2_SIZE   (sizeof(fn_actions_2) / sizeof(fn_actions_2[0]))
+#define FN_ACTIONS_3_SIZE   (sizeof(fn_actions_3) / sizeof(fn_actions_3[0]))
 
 /*
  * translates Fn keycode to action
@@ -146,9 +162,35 @@ action_t keymap_fn_to_action(uint8_t keycode)
         action.code = pgm_read_word(&fn_actions_2[FN_INDEX(keycode)]);
     }
 
+    if (layer == 3 && FN_INDEX(keycode) < FN_ACTIONS_3_SIZE) {
+        action.code = pgm_read_word(&fn_actions_3[FN_INDEX(keycode)]);
+    }
+
     if (action.code == ACTION_NO && FN_INDEX(keycode) < FN_ACTIONS_SIZE) {
         action.code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]);
     }
 
     return action;
 }
+/*
+    KEYMAP(  // LayerN: fully transparent
+        // left hand
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,TRNS,
+        // right hand
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                       TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
+*/
